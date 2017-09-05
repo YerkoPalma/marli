0f83c5fe56553f4f946bbd879c425f8f30f86e44
Yerko Palma <yerko.palma@usach.cl>
2017-09-05 17:10:32 +0000

    added highlighted example

--- example.js
+++ example.js
@@ -1,7 +1,29 @@
 var Markdown = require('.')
-var md = Markdown()
+var Highlight = require('highlight-syntax')
+var highlight = Highlight([ require('highlight-syntax/js') ])
+var css = require('sheetify')
+css('highlight-syntax/light.css')
+
+var md = Markdown({
+  highlight: function (str, lang) {
+    if (lang) {
+      try {
+        return highlight(str, { lang: lang })
+      } catch (__) {}
+    }
+
+    return ''
+  }
+})
 
 var dom = md`
 **Hello** _world_
+
+  \`\`\`js
+  var hello = 'world'
+  function print (str) {
+    console.log(str)
+  }
+  \`\`\`
 `
 document.body.appendChild(dom)
